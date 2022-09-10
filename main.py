f28806f1e419c70a0d41c6d7f27bab93c5c83233
wini83 <mariusz.wincior@gmail.com>
2022-09-10 23:09:41 +0200

    tabulated filtered list

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
-from datetime import datetime, date
+from datetime import date, timedelta
+from os import chdir, path
 from typing import List
 
 import click
-from os import chdir, path
 from loguru import logger
+from tabulate import tabulate
 
 from measurement import Measurement
 
@@ -19,16 +20,19 @@ logger.add("zepp2garmin.log", rotation="1 week")
               default=str(date.today()))
 @click.option('--date-end', type=click.DateTime(formats=["%Y-%m-%d"]),
               default=str(date.today()))
+@click.option('--height', type=int, default=-1)
 @click.option("--only_read", is_flag=True, help="Run without notifications", default=False)
-def main(file_name, date_start, date_end, only_read):
+def main(file_name, date_start, date_end, height, only_read):
+    click.echo("")
     click.echo(click.style('zepp2garmin - transfer body composition from Zepp to Garmin Connect',
                            fg='black',
                            bold=True,
                            bg="yellow",
                            blink=True))
     click.echo(f'{"=" * 120}')
-    logger.info("zepp2garmin - transfer body composition from Zepp to Garmin Connect")
-    logger.info(f"Start: {date_start}, End: {date_end} ")
+    if only_read:
+        click.echo("RUNDRY!!!")
+    click.echo(f"Start: {date_start}, End: {date_end} ")
     measurements: List[Measurement] = []
     csv_reader = csv.reader(file_name, delimiter=',')
     for row in csv_reader:
@@ -37,7 +41,32 @@ def main(file_name, date_start, date_end, only_read):
                 item = Measurement(row)
                 measurements.append(item)
 
-    logger.info(f"Measurements in file:  {len(measurements)} (total)")
+    click.echo(f"Measurements in file:  {len(measurements)} (total)")
+
+    date_end2 = date_end + timedelta(days=1)
+    date_end2 = date_end2.replace(hour=0, minute=0)
+
+    click.echo(date_end2)
+
+    filtered_list = list(filter(lambda x: date_end2 > x.timestamp > date_start, measurements))
+
+    if height != -1:
+        filtered_list = list(filter(lambda x: x.height == height, filtered_list))
+
+    click.echo(f"Measurements filtered:  {len(filtered_list)} ")
+
+    click.echo(generate_table(filtered_list))
+
+
+def generate_table(list_mes: List[Measurement]):
+    result = []
+    headers_list = ["ID", 'time', 'weight', 'height', 'bmi', 'fatRate', 'bodyWaterRate', 'boneMass', 'metabolism',
+                    'muscleRate', 'visceralFat']
+    for item in list_mes:
+        row = item.to_list()
+        row.insert(0, len(result) + 1)
+        result.append(row)
+    return tabulate(result, headers=headers_list)
 
 
 if __name__ == '__main__':
--- measurement.py
+++ measurement.py
@@ -30,25 +30,37 @@ def parse_date(string_input: str) -> datetime:
 class Measurement:
     timestamp_str: str
     weight: float = None
-    height: int
+    height: float
     bmi: float
     fatRate: float
     bodyWaterRate: float
     boneMass: float
-    metabolism: int
+    metabolism: float
     muscleRate: float
-    visceralFat: int
+    visceralFat: float
     timestamp: datetime
 
     def __init__(self, csv_line: List[str] = None):
         self.timestamp_str = csv_line[0]
         self.weight = parse_float(csv_line[1])
-        self.height = parse_int(csv_line[2])
+        self.height = parse_float(csv_line[2])
         self.bmi = parse_float(csv_line[3])
         self.fatRate = parse_float(csv_line[4])
         self.bodyWaterRate = parse_float(csv_line[5])
         self.boneMass = parse_float(csv_line[6])
-        self.metabolism = parse_int(csv_line[7])
+        self.metabolism = parse_float(csv_line[7])
         self.muscleRate = parse_float(csv_line[8])
-        self.visceralFat = parse_int(csv_line[9])
+        self.visceralFat = parse_float(csv_line[9])
         self.timestamp = parse_date(self.timestamp_str)
+
+    def to_list(self):
+        return [self.timestamp,
+                self.weight,
+                self.height,
+                self.bmi,
+                self.fatRate,
+                self.bodyWaterRate,
+                self.boneMass,
+                self.metabolism,
+                self.muscleRate,
+                self.visceralFat]
