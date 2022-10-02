9c611358940b961a7c1a86784f8842c6848b4602
wini83 <mariusz.wincior@gmail.com>
2022-10-02 08:28:42 +0200

    options tab improvements

--- options.py
+++ options.py
@@ -28,6 +28,13 @@ class OptionsFrame(ttk.Frame):
         self.usernameLabel.grid(row=0, column=0)
         self.user_passwd_label = Label(self, text="Garmin Connect Password:")
         self.user_passwd_label.grid(row=1, column=0)
+        self.date_start_label = Label(self, text="[Filter] Date Start:")
+        self.date_start_label.grid(row=3, column=0)
+        self.date_end_label = Label(self, text="[Filter] Date End:")
+        self.date_end_label.grid(row=4, column=0)
+        self.height_label = Label(self, text="[Filter] Height:")
+        self.height_label.grid(row=6, column=0)
+
 
         self.user_name_entry = ttk.Entry(self, textvariable=self.user_name_var)
         self.user_name_entry.insert(0, "user@server.com")
@@ -36,35 +43,21 @@ class OptionsFrame(ttk.Frame):
         self.user_passwd_entry = ttk.Entry(self, textvariable=self.password_var, show='*')
         self.user_passwd_entry.grid(row=1, column=1, padx=5, pady=(0, 10), sticky="ew")
 
-        self.spinbox = ttk.Spinbox(self, from_=0, to=100, increment=0.01)
-        self.spinbox.insert(0, "3.14")
-        self.spinbox.grid(row=2, column=1, padx=5, pady=10, sticky="ew")
+        self.filter_date_switch = ttk.Checkbutton(
+            self, text="Filter by date", style="Switch.TCheckbutton",)
+        self.filter_date_switch.grid(row=2, column=1, columnspan=1, pady=10)
 
-        combo_list = ["Lorem", "Ipsum", "Dolor"]
+        self.date_start_entry = ttk.Entry(self)
+        self.date_start_entry.grid(row=3, column=1, padx=5, pady=(0, 10), sticky="ew")
 
-        self.combobox = ttk.Combobox(self, values=combo_list)
-        self.combobox.current(0)
-        self.combobox.grid(row=3, column=1, padx=5, pady=10, sticky="ew")
+        self.date_end_entry = ttk.Entry(self)
+        self.date_end_entry.grid(row=4, column=1, padx=5, pady=(0, 10), sticky="ew")
 
-        self.readonly_combo = ttk.Combobox(self, state="readonly", values=combo_list)
-        self.readonly_combo.current(1)
-        self.readonly_combo.grid(row=4, column=1, padx=5, pady=10, sticky="ew")
+        self.filter_height_switch = ttk.Checkbutton(
+            self, text="Filter by height", style="Switch.TCheckbutton", )
+        self.filter_height_switch.grid(row=5, column=1, columnspan=1, pady=10)
 
-        self.menu = tkinter.Menu(self)
-        for n in range(1, 5):
-            self.menu.add_command(label=f"Menu item {n}")
+        self.height_entry = ttk.Spinbox(self, from_=80, to=250, increment=1)
+        self.height_entry.insert(0, "180.0")
+        self.height_entry.grid(row=6, column=1, padx=5, pady=10, sticky="ew")
 
-        self.menubutton = ttk.Menubutton(self, text="Dropdown", menu=self.menu)
-        self.menubutton.grid(row=5, column=0, padx=5, pady=10, sticky="nsew")
-
-        self.separator = ttk.Separator(self)
-        self.separator.grid(row=6, column=0, pady=10, sticky="ew")
-
-        self.button = ttk.Button(self, text="Click me!")
-        self.button.grid(row=7, column=0, padx=5, pady=10, sticky="ew")
-
-        self.accentbutton = ttk.Button(self, text=" I love it!", style="Accent.TButton")
-        self.accentbutton.grid(row=7, column=0, padx=5, pady=10, sticky="ew")
-
-        self.togglebutton = ttk.Checkbutton(self, text="Toggle me!", style="Toggle.TButton")
-        self.togglebutton.grid(row=8, column=0, padx=5, pady=10, sticky="nsew")
