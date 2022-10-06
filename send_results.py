f1dfae631410f9c23ae7a2d8786c689593dbe1a7
wini83 <mariusz.wincior@gmail.com>
2022-10-06 21:45:27 +0200

    code cleanup

--- send_results.py
+++ send_results.py
@@ -1,21 +1,6 @@
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
 
-
-class ButtonFrame(ttk.Frame):
-    def __init__(self, parent):
-        super().__init__(parent, style="Card.TFrame", padding=15)
-
-        self.button = None
-        self.columnconfigure(0, weight=1)
-
-        self.add_widgets()
-
-    def add_widgets(self):
-        self.button = ttk.Button(self, text="Send to GC       ", )
-        self.button.grid(row=1, column=0, padx=5, pady=10, sticky="ew")
-
-
 def format_float(float_inp):
     if float_inp is None:
         output_str = "N/A"
